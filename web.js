0f1562224334747d8992720c3d741e093086a86b
Senthil <senthilkumar_m02@infosys.com>
2013-07-23 06:44:06 +0000

    modified web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
+var fs = require('fs');
+var Buffer = require('buffer');
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(Buffer.toString(fs.readFileSync('index.html')));
 });
 
 var port = process.env.PORT || 5000;
